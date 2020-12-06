dd54517862e24b98be14714e9e49eee8334f2b86
Diffloth, Antoine (ad7073) <adiffloth@gmail.com>
2020-12-06 10:21:07 -0600

    Solved day 6

---
+++ day06/part_1.py
@@ -0,0 +1,15 @@
+groups = [x for x in open('day06/1.in').read().splitlines()]
+print(len(groups))
+
+counts = []
+uniques = set()
+for responses in groups:
+    if responses == '':
+        counts.append(len(uniques))
+        uniques.clear()
+    else:
+        for response in responses:
+            uniques.add(response)
+
+print(counts)
+print(sum(counts))
---
+++ day06/part_2.py
@@ -0,0 +1,14 @@
+groups = [x for x in open('day06/1.in').read().splitlines()]
+print(len(groups))
+
+counts = []
+group_responses = []
+for responses in groups:
+    if responses == '':
+        counts.append(len(set.intersection(*group_responses)))
+        group_responses = []
+    else:
+        group_responses.append(set(list(responses)))
+
+print(counts)
+print(sum(counts))
