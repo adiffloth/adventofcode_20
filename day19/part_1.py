43b70a4ed75cdda9c5d0fe8584dd01b375a2dd7d
Antoine Diffloth <adiffloth@gmail.com>
2021-11-26 17:06:03 -0600

    Part 1 done

--- day19/part_1.py
+++ day19/part_1.py
@@ -1,26 +1,34 @@
 import re
 
+
+# Process each rule by recursively substituting the references to other rules
+def build_re(rule_id):
+
+    rule = rules.get(rule_id)
+
+    # Base cases
+    if rule.strip('"') == ('a'):
+        return 'a'
+    if rule.strip('"') == ('b'):
+        return 'b'
+
+    # Recursive case
+    s = "".join('|' if part == '|' else build_re(part) for part in rule.split())
+    return f'({s})'
+
+
+# Build dict of rules
 rules_lst, msgs = map(str.splitlines, open('day19/0.in').read().split('\n\n'))
 rules = {}
 for rule in rules_lst:
     rule_id, rule_text = rule.split(': ')
     rules[rule_id] = rule_text
 
-
-def build_re(s):
-    if s == '|':
-        return '|'
-
-    rule = rules[s]
-    if rule.startswith('"'):
-        return rule.strip('"')
-    else:
-        return f'({"".join(build_re(part) for part in rule.split())})'
-
-
+# Build re pattern, starting with rule 0
 ptrn = re.compile(build_re('0'))
 match_count = 0
 
+# Count the messages that match the re
 for msg in msgs:
     # print(msg, ptrn.fullmatch(msg))
     if ptrn.fullmatch(msg):
